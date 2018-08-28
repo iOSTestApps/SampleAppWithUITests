31f331f5f4bd8e39e851af08a620a45f281bf618
Romain Pouclet <romain.pouclet@gmail.com>
2018-08-28 10:26:59 -0700

    Initial Commit

---
+++ SampleAppWithUITests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleAppWithUITests
+//
+//  Created by Romain Pouclet on 2018-08-28.
+//  Copyright © 2018 Buddybuild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
