8796753a1732392250f53ca30bcf14b088e8b8fb
Romain Pouclet <romain.pouclet@gmail.com>
2018-08-28 11:48:55 -0700

    Tweak post build file to generate a testing runner app with device architecture

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-find "/tmp/sandbox/$BUDDYBUILD_APP_ID" -name "*-Runner.app"
+DERIVED_DATA=$PWD/runner-app-dd
+xcodebuild build-for-testing \
+    -scheme SampleAppWithUITests \
+    -derivedDataPath "$DERIVED_DATA" \
+    -configuration Release
+
+echo "Looking for Runner app and showing architecture"
+find "$DERIVED_DATA" -name "*-Runner.app" -exec lipo -info {}/SampleAppWithUITestsUITests-Runner \;
